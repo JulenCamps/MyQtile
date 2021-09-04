b6069b0c66b10c77a922aa1d3c529a88ee5d033b
JulenCamps <julen.campsdamas@gmail.com>
2021-09-04 21:32:44 +0200

    Added new stuff

--- myqtile.py
+++ myqtile.py
@@ -1,4 +1,8 @@
 #! /bin/python3
+#JulenCamps
+#https://github.com/julencamps/myqtile
+#I'm not a professional programmer so don't expect support for
+#this script. You are running it UNDER YOUR OWN RESPONSABILITY.
 
 import os
 
@@ -28,42 +32,41 @@ def hello():
     uid = os.getuid()
 
     if uid == 0:
-        print("Warning: Do not run this script as root user!")
+        print("Warning: Do not run this script as root user. It is dangerous!")
         quit()
 
     os.system("sudo pacman -Syu")
     username = input("Introduce your system username: ")    
 
 def basic_packages():
-    os.system("sudo pacman -S install xorg lightdm lightdm-gtk-greeter qtile base-devel")
+    os.system("sudo pacman --noconfirm -S install xorg lightdm lightdm-gtk-greeter qtile base-devel | 1>/dev/null")
 
 def yay():
     global username
     os.system("sudo mkdir /opt/yay")
     os.system("sudo git clone https://aur.archlinux.org/yay.git /opt/yay")
     os.system("sudo chown {} /opt/yay".format(username))
-    os.system("cd /opt/yay && makepkg -si && cd ~/")
-    #os.system("makepkg -si") ##REVIEW!!!!!
-    #os.system("cd ~/")
+    os.system("cd /opt/yay && makepkg -si && cd")
 
 def extra_software():
     #Audio
-    os.system("sudo pacman -S pulseaudio pavucontrol")
+    os.system("sudo pacman --noconfirm -S pulseaudio pavucontrol | 1>/dev/null")
 
     #Systray applets
-    os.system("sudo pacman -S volumeicon cbatticon network-manager-applet udiskie")
+    os.system("sudo pacman --noconfirm -S volumeicon cbatticon network-manager-applet udiskie | 1>/dev/null")
 
     #Wallpaper
-    os.system("sudo pacman -S feh")
+    os.system("sudo pacman --noconfirm -S feh | 1>/dev/null")
     
     for i in software:
-        os.system("sudo pacman -S {}".format(i))
+        os.system("sudo pacman --noconfirm -S {} | 1>/dev/null".format(i))
 
 def fonts():
     os.system("yay -S ubuntu-mono-nerd-fonts")
 
 def config():
     global username
+    os.system("git clone https://github.com/julencamps/config_files ~/")
     #Qtile
     os.system("cp -r ~/config_files/.config/qtile ~/.config")
     os.system("chmod {}+x ~/.config/qtile/autostart.sh".format(username))
